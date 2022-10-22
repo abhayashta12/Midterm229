1fdfd372b50bd4a40f103ae528e04028a1350bc9
Abhay Ashta <aashta2@my.centennialcollege.ca>
2022-10-22 15:52:50 -0400

    ok

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+
+  "URI": "mongodb+srv://abhay:Openipad@cluster0.whupmzr.mongodb.net/?retryWrites=true&w=majority"
 };
